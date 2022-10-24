e39dd2434562f7bcbe0fb9557984b35ab91a71ae
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2022-10-24 09:42:00 -0700

    adds cdk plugin.

---
+++ libs/cdk/src/generators/cdk/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { CdkGeneratorSchema } from './schema';
+
+describe('cdk generator', () => {
+  let appTree: Tree;
+  const options: CdkGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ libs/cdk/src/generators/cdk/schema.d.ts
@@ -0,0 +1,5 @@
+export interface CdkGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
